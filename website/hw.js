35c34a8a566858f7fb9b77d552887f7dfa3f87e9
mvarnas <michelle.varnas@gmail.com>
2018-02-26 15:22:56 -0800

    created hw.js file

---
+++ website/hw.js
@@ -0,0 +1,19 @@
+const startStr = prompt('Enter the start')
+const start= parseInt(startStr,10)
+
+alert(start)
+
+const endStr = prompt('Enter the end')
+const end= parseInt(endStr,10)
+
+const stepStr = prompt('Enter the step')
+const step = parseInt(num,10)
+const output = document.getElementById("output")
+//     // alert('Your input is: ' + x)
+//     const output = document.getElementById("output")
+//     output.innerHTML = 'Your message is ' + message
+//     const num = prompt('Enter the step')
+//     x= parseInt(num,10)
+//     // alert('Your input is: ' + x)
+//     const output = document.getElementById("output")
+//     output.innerHTML = 'Your message is ' + message
