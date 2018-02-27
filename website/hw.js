7f22d5a57e13eb4bba1f9ea3294eec1a677fb1cd
mvarnas <michelle.varnas@gmail.com>
2018-02-27 14:56:56 -0800

    added array

--- website/hw.js
+++ website/hw.js
@@ -7,13 +7,39 @@ const endStr = prompt('Enter the end')
 const end= parseInt(endStr,10)
 
 const stepStr = prompt('Enter the step')
-const step = parseInt(num,10)
+const step = parseInt(stepStr,10)
+
+var min = start;
+var max = end;
+var increments = step;
+var arr = [];
+for (var i = 0; i <= step; ++i)
+    arr.push(min + increments * i);
+
+
+// function createNumArray(start, end):[step];
+// alert(arr)
+// {
+//     var arr = [], 
+//         i = step;
+ 
+//      while((arr[arr.length] = i) < end) {i++}
+//      return arr;
+//  }
+// alert(arr)
+// var myNumbers = [start,end];
+
+// Array.prototype.range = function(start, end) {
+//     if (!this.length) {
+//         while (end >= start) {
+//             this.push(start++);
+//         }
+//     } else {
+//         throw "You can only call 'range' on an empty array";
+//     }
+//     return this;
+// };
+
+// var array = [].range(1, 20);
 const output = document.getElementById("output")
-//     // alert('Your input is: ' + x)
-//     const output = document.getElementById("output")
-//     output.innerHTML = 'Your message is ' + message
-//     const num = prompt('Enter the step')
-//     x= parseInt(num,10)
-//     // alert('Your input is: ' + x)
-//     const output = document.getElementById("output")
-//     output.innerHTML = 'Your message is ' + message
+output.innerHTML = 'The generated array is ' + arr 
