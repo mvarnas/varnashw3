b28bfac83323ec88563f533f7b33773c52aaefde
mvarnas <michelle.varnas@gmail.com>
2018-02-27 15:24:10 -0800

    fixed array

--- website/hw.js
+++ website/hw.js
@@ -11,35 +11,9 @@ const step = parseInt(stepStr,10)
 
 var min = start;
 var max = end;
-var increments = step;
 var arr = [];
-for (var i = 0; i <= step; ++i)
-    arr.push(min + increments * i);
+for (var i = min; i <= max; i=i+step)
+    arr.push(i);
 
-
-// function createNumArray(start, end):[step];
-// alert(arr)
-// {
-//     var arr = [], 
-//         i = step;
- 
-//      while((arr[arr.length] = i) < end) {i++}
-//      return arr;
-//  }
-// alert(arr)
-// var myNumbers = [start,end];
-
-// Array.prototype.range = function(start, end) {
-//     if (!this.length) {
-//         while (end >= start) {
-//             this.push(start++);
-//         }
-//     } else {
-//         throw "You can only call 'range' on an empty array";
-//     }
-//     return this;
-// };
-
-// var array = [].range(1, 20);
 const output = document.getElementById("output")
 output.innerHTML = 'The generated array is ' + arr 
