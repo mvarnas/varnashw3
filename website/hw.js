e8987bf5c5316eca1be72d6c582efc68bb9e7e84
mvarnas <michelle.varnas@gmail.com>
2018-02-27 23:58:18 -0800

    commented out

--- website/hw.js
+++ website/hw.js
@@ -5,29 +5,28 @@ const endStr = prompt('Enter the end')
 const end= parseInt(endStr,10)
 
 const stepStr = prompt('Enter the step')
-    if (stepStr==0) or (stepStr==isNaN); {
-        document.write('Invalid integer input, please reload and try again.');
-    }
-const step = parseInt(stepStr,10)
+//     if (stepStr==0) or (stepStr==isNaN); {
+//        alert('Invalid integer input, please reload and try again');
+// }
+// // else {
+    const step = parseInt(stepStr,10)
 
-var min = start;
-var max = end;
-var arr = [];
-for (var i = min; i <= max; i=i+step)
-    arr.push(i);
+    var min = start;
+    var max = end;
+    var arr = [];
+        for (var i = min; i <= max; i=i+step){
+            arr.push(i);
+        }
+    var total=0;
+        for (var i in arr){
+        total +=arr[i];
+        }
 
-var total=0;
-for (var i in arr){
-total +=arr[i];
-}
-
-var binArr =[];
-for (var i=0, len=arr.length; i<len; i++) {
-    total+=arr[i]; binArr.push(arr[i].toString(2))
-}
+    var binArr =[];
+        for (var i=0, len=arr.length; i<len; i++) {
+        total+=arr[i]; binArr.push(arr[i].toString(2))
+        }
 
 document.write('The generated array is ' + arr  + '<br>') ; 
 document.write('The sum is ' + total + '<br>' ); 
 document.write('The binary of absolute element values are: ' + binArr);
-// const output2 = document.getElementById("output2")
-// output2.innerHTML = 'The sum is ' + total;
